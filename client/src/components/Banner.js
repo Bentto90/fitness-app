fc52cee27ceec7c39236e6b15437184687ac3b08
bentto90 <cch3056@gmail.com>
2023-06-12 16:21:14 -0700

    stack rmved

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Stack, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 import HeroBannerImage from '../assets/banner.png';
 
