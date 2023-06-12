5cd2515d87291009c933a57b781b553769a35c81
dlexd <Derrekle@gmail.com>
2023-06-12 10:03:35 -0700

    Added WGER workoutscheduler and exercise search

---
+++ client/src/pages/Exercises.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ExerciseSearch from '../components/Exercises';
+
+const Exercises = () => {
+  return (
+    <div>
+      <h1>Exercise Search Page</h1>
+      <ExerciseSearch />
+    </div>
+  );
+};
+
+export default Exercises;
