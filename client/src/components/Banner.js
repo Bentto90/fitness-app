699d7aca85f9491b98df645a123fe23b245479fb
bentto90 <cch3056@gmail.com>
2023-06-12 13:56:25 -0700

    finished

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
-import HeroBannerImage from '../assets/images/banner.png';
+import HeroBannerImage from '../assets/banner.png';
 
 const HeroBanner = () => (
-  <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
-    <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
-    <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
-      Sweat, Smile <br />
-      And Repeat
+  <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' }, position: 'relative' }} p="20px">
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box sx={{ flex: 1 }}>
+        <Typography color="#434ee8" fontWeight="600" fontSize="100px">Be Fit not Fat!</Typography>
+        <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
+          Fitness <br />
+          Made Easy
+        </Typography>
+        <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
+          Join today and start your fitness journey with us.
+        </Typography>
+      </Box>
+      <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
+        <img
+          src={HeroBannerImage}
+          alt="hero-banner"
+          className="hero-banner-img"
+          style={{ width: '100%', maxWidth: '400px' }}
+        />
+      </Box>
+    </Box>
+    <Typography fontWeight={600} color="#434ee8" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
+        Fit For Life
     </Typography>
-    <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
-      Check out the most effective exercises personalized to you
-    </Typography>
-    <Stack>
-      <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#FF2625', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
-    </Stack>
-    <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
-      Exercise
-    </Typography>
-    <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
   </Box>
 );
 
